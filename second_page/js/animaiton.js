b1c32e95c9deb53a1b1119eb6fa36dc2d060e1f8
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-05-11 21:24:37 +0200

    animation

--- second_page/js/animaiton.js
+++ second_page/js/animaiton.js
@@ -5,16 +5,17 @@ let settings = document.querySelector(".settings");
 
 let open = 1;
 
+settings.onclick = openPanel = () => {
+    gsap.to(".s_panel", { duration: .5, x: -400 });
+    gsap.to('.s_panel_close_btn', { delay: .5, opacity: 1 });
+    gsap.to('.settings', { duration: .5, ease: "power4.out", opacity: 0 })
+}
+
 s_panel_close_btn.onclick = closePanel = () => {
-    gsap.to(".s_panel", { duration: .5, opacity: 0, x: 200});
+    gsap.to(".s_panel", { duration: .5, x: 200});
     gsap.to('.s_panel_close_btn', { opacity: 0},"-=1");
     gsap.to('.settings', { duration: .5, ease: "power4.out", opacity: 1 });
 }
 
-settings.onclick = openPanel = () => {
-    gsap.to(".s_panel", { duration: .5, opacity: 1, x: 0 });
-    gsap.to('.s_panel_close_btn', { delay: .5, opacity: 1 });
-    gsap.to('.settings', { duration: .5, ease: "power4.out", opacity: 0 })
-}
 
 
